3978e216f96fba91e83c0ad48de78399b91bcd5e
Eric Niemeyer <niemeyer.eric@gmail.com>
2025-05-26 18:50:13 -0400

    pre token component

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -5,16 +5,19 @@ interface TokenProps {
   token: TokenData;
   isSelected: boolean;
   onClick: (tokenId: number) => void;
+  tokenType: 'token_training' | 'token_input' | 'token_output';
 }
 
 const Token: React.FC<TokenProps> = ({ token, isSelected, onClick }) => {
   return (
-    <div 
+    <div
       className={`
         px-3 py-2 rounded-md cursor-pointer transition-all
-        ${isSelected 
-          ? 'bg-blue-500 text-white shadow-lg transform scale-110' 
-          : 'bg-gray-100 hover:bg-gray-200'}
+        ${
+          isSelected
+            ? 'bg-blue-500 text-white shadow-lg transform scale-110'
+            : 'bg-gray-100 hover:bg-gray-200'
+        }
       `}
       onClick={() => onClick(token.id)}
     >
